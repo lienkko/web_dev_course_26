029571b442968e5a278390c214a9fb3cc20f08f3
Artem Lienko <tema.nesmeyanow@yandex.ru>
2026-02-11 20:10:26 +0300

    completed lab1

--- labs/lab1/c1_control_statements.rb
+++ labs/lab1/c1_control_statements.rb
@@ -10,7 +10,13 @@
 # Expected: check_number(5) => "positive"
 def check_number(num)
   # TODO: Use if/elsif/else statement
-  nil
+  if num > 0
+    return "positive"
+  elsif num < 0
+    return "negative"
+  else
+    return "zero"
+  end
 end
 
 # Exercise 2: Ternary operator
@@ -18,7 +24,7 @@ end
 # Expected: even_or_odd(4) => "even"
 def even_or_odd(num)
   # TODO: Use condition ? true_value : false_value
-  nil
+  return num %2 == 0 ? "even" : "odd"
 end
 
 # Exercise 3: Unless statement (Ruby-specific)
@@ -27,7 +33,10 @@ end
 # Expected: check_access(15) => "access denied"
 def check_access(age)
   # TODO: Use unless statement (opposite of if)
-  nil
+  unless age < 18
+    return "access granted"
+  end
+  return "access denied"
 end
 
 # Exercise 4: Case statement (switch)
@@ -35,7 +44,14 @@ end
 # Expected: day_type("Monday") => "weekday"
 def day_type(day)
   # TODO: Use case/when statement
-  nil
+  case day
+  when "Monday","Tuesday","Wednesday","Thursday","Friday"
+    return "weekday"
+  when "Saturday","Sunday"
+    return "weekend"
+  else
+    return "invalid"
+  end
 end
 
 # Exercise 5: While loop
@@ -43,6 +59,13 @@ end
 # Expected: count_up(5) => [1, 2, 3, 4, 5]
 def count_up(n)
   # TODO: Use while loop
+  arr =[]
+  i = 1
+  while i<= n
+    arr.push(i)
+    i=i+1
+  end
+  return arr
 end
 
 # Exercise 6: Until loop (Ruby-specific)
@@ -50,7 +73,13 @@ end
 # Expected: count_down(5) => [5, 4, 3, 2, 1]
 def count_down(n)
   # TODO: Use until loop (opposite of while)
-  nil
+  i = n
+  arr = []
+  until i<=0
+    arr << i
+    i=i-1
+  end
+  return arr
 end
 
 # Exercise 7: For loop / each iterator
@@ -58,15 +87,26 @@ end
 # Expected: sum_with_loop([1, 2, 3, 4]) => 10
 def sum_with_loop(arr)
   # TODO: Use for loop
-  nil
+  sum = 0
+  for el in arr
+    sum += el
+  end
+  return sum
 end
-
 # Exercise 8: Break and next
 # Return an array of numbers from array that are < 10, stop at first number >= 20
 # Expected: process_numbers([5, 8, 12, 25, 3, 15]) => [5, 8, 12]
 def process_numbers(arr)
   # TODO: Use break to stop, next to skip
-  nil
+  arr2 = []
+  for el in arr 
+    if el >= 20
+      break
+    else
+      arr2.push(el)
+    end
+  end
+  return arr2
 end
 
 # Exercise 9: Modifier if/unless
@@ -76,7 +116,7 @@ end
 # Expected: check_size(50) => 50
 def check_size(num)
   # TODO: Use: return "big" if num > 100; return num
-  nil
+  return "big" if num > 100;return num 
 end
 
 # ============================================
--- labs/lab1/c2_arrays.rb
+++ labs/lab1/c2_arrays.rb
@@ -10,7 +10,9 @@
 # Expected: first_and_last([1, 2, 3, 4]) => [1, 4]
 def first_and_last(arr)
   # TODO: Use array indexing with [0] and [-1]
-  nil
+  arr2 = []
+  arr2.push(arr[0])
+  arr2.push(arr[-1])
 end
 
 # Exercise 2: Array methods
@@ -18,7 +20,7 @@ end
 # Expected: add_element([1, 2, 3], 4) => [1, 2, 3, 4]
 def add_element(arr, element)
   # TODO: Use .push or << operator (modify and return the array)
-  nil
+  arr << element
 end
 
 # Exercise 3: Array iteration
@@ -26,7 +28,7 @@ end
 # Expected: double_numbers([1, 2, 3]) => [2, 4, 6]
 def double_numbers(arr)
   # TODO: Use .map method
-  nil
+  return arr.map{|el|el*2}
 end
 
 # Exercise 4: Array filtering
@@ -34,7 +36,7 @@ end
 # Expected: even_numbers([1, 2, 3, 4, 5, 6]) => [2, 4, 6]
 def even_numbers(arr)
   # TODO: Use .select or .filter method
-  nil
+  return arr.filter{|num| num.even?}
 end
 
 # Exercise 5: Array reduction
@@ -42,23 +44,22 @@ end
 # Expected: sum_array([1, 2, 3, 4]) => 10
 def sum_array(arr)
   # TODO: Use .sum or .reduce method
-  nil
-end
+  arr2 = arr.sum
+end;
 
 # Exercise 6: Array operations
 # Check if an array contains a specific element
 # Expected: contains?([:a, :b, :c], :b) => true
 def contains?(arr, element)
   # TODO: Use .include? method
-  nil
+  return arr.include?(element)
 end
 
 # Exercise 7: Array sorting
 # Sort an array of numbers in descending order
 # Expected: sort_desc([3, 1, 4, 1, 5]) => [5, 4, 3, 1, 1]
 def sort_desc(arr)
-  # TODO: Use .sort with .reverse or .sort_by
-  nil
+  arr.sort_by {|item| -item}
 end
 
 # Exercise 8: Array range
@@ -66,7 +67,7 @@ end
 # Expected: number_range(1, 5) => [1, 2, 3, 4, 5]
 def number_range(start, finish)
   # TODO: Use (start..finish).to_a
-  nil
+  return (start..finish).to_a
 end
 
 # ============================================
